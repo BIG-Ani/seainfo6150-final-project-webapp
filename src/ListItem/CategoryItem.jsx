dc4b0930b03b053c0d8c08b3fc7a22951e647ef2
Leichen Zhou <leichenzhou@Leichens-MacBook-Pro.local>
2020-12-11 18:45:27 -0800

    fix link bug

--- src/ListItem/CategoryItem.jsx
+++ src/ListItem/CategoryItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LinesEllipsis from "react-lines-ellipsis";
+import {Link} from "react-router-dom";
 
 import styles from './CategoryItem.module.css'
 
@@ -7,9 +8,6 @@ const CategoryItem = (props) => {
 
     const posts = props.posts;
     const brand = props.brand;
-
-    console.log('brand', brand)
-    console.log('posts', posts)
 
     return(
 
@@ -23,7 +21,9 @@ const CategoryItem = (props) => {
                     .map((post) => (
                         <div className={styles.Post} key={post.id}>
                             {/*<h3><a href={'/seainfo6150-final-project-webapp/posts/'+post.id}>{post.title}</a></h3>*/}
-                            <h3><a href={'/posts/'+post.id}>{post.title}</a></h3>
+                            <h3>
+                                <Link href={'/posts/'+post.id}>{post.title}</Link>
+                            </h3>
 
                             <p>- {post.author}</p>
                             <div>
